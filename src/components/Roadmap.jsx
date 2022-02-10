136d871a3d76048db6348af1aa113acbf0a2b597
SeanConnorz <seanandhughes@gmail.com>
2022-02-10 16:53:25 -0500

    Rough template of roadmap

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -2,12 +2,17 @@ import "../styles/Roadmap.css";
 
 export default function Roadmap() {
   return (
-    <section className="w-full">
-      <h1 className="flex justify-center bg-[url('../public/images/TV.png')] bg-center bg-no-repeat bg-contain w-full h-[50rem]">
+    <section className="w-[80%]">
+      <div className="flex justify-center items-center bg-[url('../public/images/TV.png')] bg-center bg-no-repeat bg-contain w-full h-[50rem] text-center">
         <div>
-          <h1>Roadmap</h1>
+          <h1 className="relative bottom-[15vh] text-[2rem]">Roadmap</h1>
+          <ul className="grid grid-cols-1 gap-4">
+            <li>NFT release</li>
+            <li>Game Creation</li>
+            <li>Tokenomics</li>
+          </ul>
         </div>
-      </h1>
+      </div>
     </section>
   );
 }
