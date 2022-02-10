5d425b44298f5fc19fecf16a3c818e8cc36830b3
SeanConnorz <seanandhughes@gmail.com>
2022-02-10 17:03:50 -0500

    Finshing rough front end template

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,7 +1,7 @@
 export default function Team() {
   return (
-    <section className="flex flex-col my-[20rem]">
-      <h1 className="text-[3rem]">Meet the Team... or person</h1>
+    <section className="flex flex-col items-center mt-[20rem] mb-[5rem]">
+      <h1 className="text-[3rem] mb-[8rem]">Meet the Team... or person</h1>
       <div className="max-w-sm rounded overflow-hidden shadow-lg">
         <img
           className="w-full"
