2423eb2d0b87331cd6f46491c00e95354a06d823
SeanConnorz <seanandhughes@gmail.com>
2022-02-15 12:17:23 -0500

    Refactoring character movement and adding walls

--- src/components/Metaverse/logic/Person.js
+++ src/components/Metaverse/logic/Person.js
@@ -16,26 +16,39 @@ export class Person extends GameObject {
   }
 
   update(state) {
-    this.updatePosition();
-    this.updateSprite(state);
+    if (this.movingProgressRemaining > 0) {
+      this.updatePosition();
+    } else {
+      // More cases for starting to walk will come here
 
-    if (
-      this.isPlayerControlled &&
-      this.movingProgressRemaining === 0 &&
-      state.arrow
-    ) {
-      this.direction = state.arrow;
-      console.log(state.map.isSpaceTaken(this.x, this.y, this.direction));
+      // Case: we're keyboard ready and have an arrow pressed
+      if (this.isPlayerControlled && state.arrow) {
+        this.startBehavior(state, {
+          type: "walk",
+          direction: state.arrow,
+        });
+      }
+      this.updateSprite(state);
+    }
+  }
+
+  startBehavior(state, behavior) {
+    // Setting character direction to whatever behavior has
+    this.direction = behavior.direction;
+
+    if (behavior.type === "walk") {
+      // Stop here if space is not free
+      if (state.map.isSpaceTaken(this.x, this.y, this.direction)) {
+        return;
+      }
       this.movingProgressRemaining = 16;
     }
   }
 
   updatePosition() {
-    if (this.movingProgressRemaining > 0) {
-      const [property, change] = this.directionUpdate[this.direction];
-      this[property] += change;
-      this.movingProgressRemaining -= 1;
-    }
+    const [property, change] = this.directionUpdate[this.direction];
+    this[property] += change;
+    this.movingProgressRemaining -= 1;
   }
 
   updateSprite(state) {
