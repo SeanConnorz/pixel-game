41e8d2fa7322fba4fe4727bc38035527c31c4a35
SeanConnorz <seanandhughes@gmail.com>
2022-02-17 16:57:35 -0500

    Making roadmap responsive

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -2,10 +2,10 @@ import "../styles/Roadmap.css";
 
 export default function Roadmap() {
   return (
-    <section className="w-[80%]">
-      <div className="flex justify-center items-center bg-[url('../public/images/TV.png')] bg-center bg-no-repeat bg-contain w-full h-[50rem] text-center">
+    <section className="flex flex-col items-center w-[80%]">
+      <h1 className="relative text-[2rem] mb-[2rem]">Roadmap</h1>
+      <div className="televesion flex justify-center items-center bg-[url('../public/images/TV.png')] bg-center bg-no-repeat bg-contain w-full text-center">
         <div>
-          <h1 className="relative bottom-[15vh] text-[2rem]">Roadmap</h1>
           <ul className="grid grid-cols-1 gap-4">
             <li>NFT release</li>
             <li>Game Creation</li>
